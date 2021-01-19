8225715d8b7762ed02c908009e475b3c4f0970f4
Pengfei Zhu <zhuzhuyule@126.com>
2021-01-19 10:24:55 +0800

    add eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'aribnb-typescript',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+};
