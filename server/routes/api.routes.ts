362a334af726fcdd567472a0869ea61af65dc6ca
Pengfei Zhu <zhuzhuyule@126.com>
2021-01-20 13:58:37 +0800

    refactor api source files

--- server/routes/api.routes.ts
+++ server/routes/api.routes.ts
@@ -1,48 +1,9 @@
+import { findDoubanId, findIMDBId, findSun, searchMovie } from '@services/operate';
 import express from 'express'
 
-import movieCtrl from '@controllers/movie.controller';
-import proxyCtrl from '@controllers/proxy.controller';
+const router = express.Router();
 
-const router = express.Router() 
-router.get('/douban/:id', findDoubanId)
+router.get('/douban/:id', findDoubanId);
 router.get('/imdb/:id', findIMDBId);
 router.get('/search', searchMovie);
 router.get('/proxy/sun', findSun);
-
-async function findDoubanId(req: any, res: { json: (_: any) => void }) {
-    const movie = await movieCtrl.findOneById(req.params.id);
-    res.json(movie)
-}
-
-async function findIMDBId(req: any, res: { json: (_: any) => void }) {
-  const movie = await movieCtrl.findOne({
-    IMDb: req.params.id
-  });
-  res.json(movie);
-}
-async function searchMovie(req: any, res: any) {
-  console.log(req);
-  const { name, id, imdb, year, alias, actor } = req.query;
-  const movie = await movieCtrl.find({
-    ...(name && { title: new RegExp(name) }),
-    ...(id && { id: parseInt(id, 10) }),
-    ...(imdb && { IMDb: imdb }),
-    ...(year && { year: parseInt(year, 10) }),
-    ...(alias && { alias: new RegExp(alias) }),
-    ...(actor && { actor: new RegExp(actor) }),
-  });
-  res.json(movie);
-}
-
-async function findSun(req: any, res: any) {
-  const { top = 10, h = 0, m = 0, date = '', valid = false } = req.query;
-  let proxies;
-  if (valid) {
-    proxies = await proxyCtrl.getValidProxies('sun');
-  } else {
-    proxies = await proxyCtrl.getProxies({ type: 'sun', top: parseInt(top), hour: parseInt(h), min: parseInt(m), baseDate: date});
-  }
-  res.json(proxies);
-}
-
-export default router
---
+++ server/services/operate.ts
@@ -0,0 +1,41 @@
+import movieCtrl from '@controllers/movie.controller';
+import proxyCtrl from '@controllers/proxy.controller';
+import express from 'express';
+
+export async function findDoubanId(req: express.Request, res: express.Response): Promise<void>  {
+  const movie = await movieCtrl.findOneById(req.params.id);
+  res.json(movie)
+}
+
+export async function findIMDBId(req: express.Request, res: express.Response): Promise<void>  {
+  const movie = await movieCtrl.findOne({
+    IMDb: req.params.id
+  });
+  res.json(movie);
+}
+
+export async function searchMovie(req: express.Request, res: express.Response): Promise<void>  {
+  console.log(req);
+  const { name, id, imdb, year, alias, actor } = req.query;
+  const movie = await movieCtrl.find({
+    ...(name && { title: new RegExp(name.toString()) }),
+    ...(id && { id: parseInt(id.toString(), 10) }),
+    ...(imdb && { IMDb: imdb }),
+    ...(year && { year: parseInt(year.toString(), 10) }),
+    ...(alias && { alias: new RegExp(alias.toString()) }),
+    ...(actor && { actor: new RegExp(actor.toString()) }),
+  });
+  res.json(movie);
+}
+
+export async function findSun(req: express.Request, res: express.Response): Promise<void> {
+  const { top = 10, h = 0, m = 0, date = '', valid = false } = req.query;
+  let proxies;
+  if (valid) {
+    proxies = await proxyCtrl.getValidProxies('sun', parseInt(top.toString()));
+  } else {
+    proxies = await proxyCtrl.getProxies({ type: 'sun', top: parseInt(top.toString()), hour: parseInt(h.toString()), min: parseInt(m.toString()), baseDate: date.toString() });
+  }
+  res.json(proxies);
+}
+
