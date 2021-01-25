3a0bf9ce96ab224b319b74914cf36af7a2d60fdd
Pengfei Zhu <zhuzhuyule@126.com>
2021-01-25 10:31:29 +0800

    fix add ip to whitelist url error issue

--- server/configs/customer.config.js
+++ server/configs/customer.config.js
@@ -4,7 +4,7 @@ export default {
     password: '住23123',
     packageNum: '62031',
     defaultToken: 'nl63f2iv7jspj0fu59kkisbu97',
-    whitelistUrl: 'ty-http-d.hamir.net/index/white/add?neek=tyhttp761091&appkey=ff0109d1efaa15146b6830021a51a162&white=',
+    whitelistUrl: 'http://ty-http-d.hamir.net/index/white/add?neek=tyhttp761091&appkey=ff0109d1efaa15146b6830021a51a162&white=',
   },
   freeProxy: {
     baseUrl: 'http://127.0.0.1:5010',
