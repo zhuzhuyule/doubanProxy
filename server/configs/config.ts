8a75cc53f0680cfaf7c0e6850419b37bfbae3192
Pengfei Zhu <zhuzhuyule@126.com>
2021-02-04 13:17:13 +0800

    read mongodb config from envrionment

--- server/configs/config.ts
+++ server/configs/config.ts
@@ -1,10 +1,10 @@
 import customerConfig from './customer.config';
 export default {
-  port: 3000,
+  port: process.env.SERVER_PORT || 3000,
   env: '',
   mongo: {
-    host: 'mongodb://localhost/douban',
-    port: 27017,
+    host: process.env.MONGODB_HOST || 'mongodb://127.0.0.1/douban',
+    port: process.env.MONGODB_PORT || 27017,
     isDebug: false,
   },
   ...customerConfig
